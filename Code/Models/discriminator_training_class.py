e65fa484c2ff783b6576e59820fdd73e045eb002
daniel.stancl@gmail.com <stancld@Danko-Notebok.localdomain>
2020-03-15 16:43:06 +0100

    debugging

--- Code/Models/discriminator_training_class.py
+++ Code/Models/discriminator_training_class.py
@@ -100,13 +100,12 @@ class Discriminator_utility():
         self.model.train()
         epoch_loss = 0
         for local_batch, local_labels in self._generate_batches(X_train, y_train):
-            print(torch.cuda.memory_summary())
             
             # pass through embedding layer
             local_batch_embedded = self._embedding_layer(local_batch)
             # -> [batch_size,seq_len,emb_dim]
 
-            local_batch, local_labels = local_batch.to(
+            local_batch_embedded, local_labels = local_batch_embedded.to(
                 self.device), local_labels.flatten().to(self.device)
 
             # print('input are:')
@@ -114,9 +113,7 @@ class Discriminator_utility():
             # print(local_labels.size())
 
             self.optimiser.zero_grad()
-            local_output = F.softmax(
-                self.model(local_batch_embedded)
-                )
+            local_output = self.model(local_batch_embedded)
 
             # print('output are:')
             # print(local_output.size())
@@ -146,7 +143,7 @@ class Discriminator_utility():
             local_batch_embedded = self._embedding_layer(local_batch)
             # -> [batch_size,seq_len,emb_dim]
 
-            local_batch, local_labels = local_batch.to(
+            local_batch_embedded, local_labels = local_batch_embedded.to(
                 self.device), local_labels.flatten().to(self.device)
 
             local_output = self.model(local_batch_embedded)
