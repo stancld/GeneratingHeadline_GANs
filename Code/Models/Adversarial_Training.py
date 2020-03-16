8f0016884f193f2927977941d4a7dcb56f4fc77d
daniel.stancl@gmail.com <stancld@Danko-Notebok.localdomain>
2020-03-16 18:06:40 +0100

    adversarial fiddling

--- Code/Models/Adversarial_Training.py
+++ Code/Models/Adversarial_Training.py
@@ -267,7 +267,7 @@ class AdversarialTraining:
                     error_G_1 = self.loss_function_D(output_D_G, real_labels_flatten)
                     error_G = error_G_1 * error_G_2
                     # Calculate gradient
-                    error_G.backward()
+                    error_G.backward(retain_graph = True)
                     # Update step
                     self.optimiser_G.step()
                 
