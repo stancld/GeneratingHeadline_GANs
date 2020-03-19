285f738619c1949c4c379dd54ed6d752057bd857
daniel.stancl@gmail.com <stancld@Danko-Notebok.localdomain>
2020-03-19 08:29:35 +0100

    GAN loss w.r.t. G

--- Code/Models/Adversarial_Training.py
+++ Code/Models/Adversarial_Training.py
@@ -267,7 +267,7 @@ class AdversarialTraining:
                     error_G_2 = self.loss_function_G(output_G[0], target_padded[0])
                     error_G_1 = self.loss_function_D(output_D_G, real_labels_flatten)
                     error_G = error_G_1 * error_G_2
-                    print(f'BCELoss = {error_G_1:.3f}, Cross-Entropy = {error_G_2:.3f}')
+                    
                     # Calculate gradient
                     error_G.backward(retain_graph = True)
                     # Update step
@@ -275,6 +275,7 @@ class AdversarialTraining:
                     # cleaning
                     del output_G, target_padded
                     torch.cuda.empty_cache()
+                print(f'BCELoss = {error_G_1:.3f}, Cross-Entropy = {error_G_2:.3f}')
                 
                 #### MEASUREMENT ####
                 if batch % 20 == 0:
