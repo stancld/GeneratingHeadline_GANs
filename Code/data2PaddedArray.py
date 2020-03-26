b4e44eac716160641e04f9ee9068fe93d686c4b7
daniel.stancl@gmail.com <stancld@Danko-Notebok.localdomain>
2020-03-26 15:30:07 +0100

    commented data2PaddedArray.py

--- Code/data2PaddedArray.py
+++ Code/data2PaddedArray.py
@@ -18,41 +18,47 @@ import numpy as np
 def data2PaddedArray(input, target, text_dictionary:dict, embeddings):
     """
     :param input:
-        type:
-        description:
+        type: Numpy.Object: [n_examples]
+        description: Sequences of input articles
     :param target:
-        type:
-        description
-        
-    :return embedded_matrix:
-        type: numpy.array
-        description:
+        type: Numby.Object: [n_examples]
+        description Sequences of tartget summaries
+    :param text_dictionary:
+        type: Dictionary
+        description: Dictionary containing text_dictionary intended for input articles and headline dictionary used for target summaries
+    ##
+    :param embeddings: !! This parameter is to left here unintentionally from some preliminary versions. Desired to be deleted if some further updates are done in future.
+    ##
+         
+    :return padded_input:
+        type: Numpy.Array: [seq_Length, n_examples]
+        description: Padded sequences of input articles
     :return input_seq_lengths:
-        type: numpy.array
-        description:
+        type: Numpy.Array: [n_examples]
+        description: An array containing lengths of input articles
     :return padded_target:
-        type: numpy.array
-        description:
+        type: Numpy.Array: [seq_Length, n_examples]
+        description: Padded sequences of target summaries
     :return target_seq_lengths:
-        type: numpy.array
-        description
+        type: Numpy.Arrat: [n_examples]
+        description: An array containing lengths of target summaries
     """
     # HELPER function
     def __word2index__(word, text_dictionary, embeddings = embeddings):
       """
       :param word:
-          type:
-          description:
+          type: String
+          description: A single word distilled from the input/target text
       :param text_dictionary:
-          type:
-          description:
-      :param embeddings:
-          type:
-          description:
+          type: Dictionary
+          description: A dictionary containing two other dictionary - text_dictionary intended for input artciles and headline_dictionary for targer summaries
+      ##
+      :param embeddings: !! This parameter is to left here unintentionally from some preliminary versions. Desired to be deleted if some further updates are done in future.
+      ##
               
-      :return word2index:
-          type:
-          description:  
+      :return word2index: 
+          type: Integer
+          description: Index to a given word (or unknown word) 
       """
       try:
         word2index = text_dictionary.word2index[word]
@@ -68,9 +74,13 @@ def data2PaddedArray(input, target, text_dictionary:dict, embeddings):
         [[__word2index__(word, text_dictionary = text_dictionary['headline_dictionary']) for word in sentence] for sentence in target]
         )
     
-    ### Convert the input data to embedded representation
+    ### Pad the input articles
+    # Derive the length of the longest input article
     max_lengths = np.array([len(sentence) for sentence in input]).max()
+    # create an empty list for storing padded input articles and their original lengths
     padded_input, input_seq_lengths = [], []
+    
+    # Pad and store article by article from the input
     for sentence in numericalVec_input:
         input_seq_lengths.append(len(sentence))
         if len(sentence) == max_lengths:
@@ -81,13 +91,17 @@ def data2PaddedArray(input, target, text_dictionary:dict, embeddings):
                              np.repeat(pad_idx, max_lengths - len(sentence)).reshape((1, -1))
                              ]
         padded_input.append(sentence)
-
+    # Put input_seq_lengths to Numpy array.
     input_seq_lengths = np.array(input_seq_lengths, np.int)
     del numericalVec_input 
 
     ### Pad the target data
+    # Derive the length of the longest target summaries
     max_lengths = np.array([len(sentence) for sentence in target]).max()
+    # create an empty list for storing padded target summaries and their original lengths
     padded_target, target_seq_lengths = [], []
+    
+    # Pad and store summary by summary from the target
     for sentence in numericalVec_target:
         target_seq_lengths.append(len(sentence))
         if len(sentence) == max_lengths:
@@ -98,13 +112,13 @@ def data2PaddedArray(input, target, text_dictionary:dict, embeddings):
                              np.repeat(pad_idx, max_lengths - len(sentence)).reshape((1, -1))
                              ]
         padded_target.append(sentence)
-    
+    # Put target_seq_lengths to Numpy array.
     target_seq_lengths = np.array(target_seq_lengths, np.int)
-    
     del numericalVec_target
     
-    return (np.array(padded_input, np.int32).squeeze(1).swapaxes(0,1), # => dims: [seq_length, n_examples, embedded_dim]
+    # return
+    return (np.array(padded_input, np.int32).squeeze(1).swapaxes(0,1), # => dims: [seq_length, n_examples]
             input_seq_lengths,
-            np.array(padded_target, np.int32).squeeze(1).swapaxes(0,1), # => dims: [seq_length, n_examples,]
+            np.array(padded_target, np.int32).squeeze(1).swapaxes(0,1), # => dims: [seq_length, n_examples]
             target_seq_lengths,
             )
